bafcd3b1c5722424308ac302c89ab92d21250459
Erik De Rijcke <derijcke.erik@gmail.com>
2016-11-20 00:00:28 +0100

    experiment with object creation and architecture

--- app/westfield-core.js
+++ app/westfield-core.js
@@ -3,51 +3,95 @@
 //TODO wire protocol parser & handler
 
 //define westfield namespace as wf
-var wf = wf || {};
+const wf = wf || {};
 
-//westfield display "class"
-var Display = function () {
+//TODO display scoped object id generator
 
-    //display object always has id 1.
-    this.id = 1;
+wf.connect = function (url) {
+    const socket = new WebSocket(url, "westfield");
+    return new Display(1, socket);
+}
 
-    //--public--
-    this.connect = function (url) {
-        //TODO create & configure websocket
-        this.socket = setupSocket(url);
+wf.connectToSocket = function (webSocket) {
+    return new Display(1, webSocket);
+}
+
+wf.Proxy = function (objectId) {
+    Object.defineProperty(this, "objectId", {
+        value: objectId,
+        writable: false
+    });
+};
+
+wf.Registry = function (id) {
+    //--constructor--
+    Proxy.call(this, id);
+
+    //more empty public functions are added by the protocol generator
+}
+wf.Registry.prototype = Object.create(wf.Proxy.prototype);
+
+wf.Dispatcher = function (nextObjectid) {
+    this.onWireMessage = function (blob) {
+        //TODO parse blob
+        //TODO find proxy object and call it
     };
 
+    //--constructor--
+    const nextObjectId = nextObjectid;
+};
+
+//westfield display core functionality
+wf.Display = function (id, webSocket) {
+
+    //--object management--
+    const proxyRegistry = new Map();
+    var lastObjectId = 1;
+    const nextObjectId = function () {
+        return lastObjectId++;
+    };
+
+    //--public properties--
+    Object.defineProperty(this, "registry", {
+        value: new wf.Registry(nextObjectId()),
+        writable: false
+    });
+
+    //--private properties--
+    const dispatcher = new Dispatcher(nextObjectId);
+
+    //--public functions--
     this.disconnect = function () {
+        socket.close();
+    };
+
+    //more empty public functions are added by the protocol generator
+
+    //--private functions--
+    const onSocketOpen = function (event) {
+        //TODO send back-end minimal required browser info (we start with screen size)
+    };
+
+    const onSocketClose = function (event) {
 
     };
 
-    //--private--
-    var setupSocket = function (url) {
-        var socket = new WebSocket(url, "westfield");
+    const onSocketError = function (event) {
+
+    };
+
+    const setupSocket = function (socket) {
         socket.onopen = onSocketOpen;
-        socket.onmessage = onSocketMessage;
         socket.onclose = onSocketClose;
         socket.onerror = onSocketError;
+
+        socket.onmessage = dispatcher.onWireMessage;
 
         return socket;
     };
 
-    var onSocketOpen = function (event) {
-
-    };
-
-    var onSocketMessage = function (event) {
-
-    }
-
-    var onSocketClose = function (event) {
-
-    }
-
-    var onSocketError = function (event) {
-
-    }
+    //--constructor--
+    Proxy.call(this, id);
+    const socket = setupSocket(webSocket);
 };
-
-//westfield display singleton
-wf.Display = new Display();
+wf.Display.prototype = Object.create(wf.Proxy.prototype);
