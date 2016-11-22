3eadb87d5a27cada0f2d4bb6e0becbfe97250942
Erik De Rijcke <derijcke.erik@gmail.com>
2016-11-22 23:09:04 +0100

    implement marshalling logic & experiment with generated code format

--- app/westfield-core.js
+++ app/westfield-core.js
@@ -5,52 +5,58 @@
 const wf = wf || {};
 
 wf._Object = function (connection) {
+
     //--public functions--
     this.delete = function () {
         this.objects.remove(this._id);
-        connection.marshall(this._id, 0, "o", [object._id]);
+        connection.marshall(this._id, 0, []);//opcode 0 is reserved for deletion
     };
 
     this.postError = function (errorCode, errorMsg) {
-        //TODO send error on connection
+        connection.marshallTo(this._id, 255, [//opcode 255 is reserved for error
+            {
+                value: errorCode,
+                type: "i",
+                size: 4,
+                optional: false,
+                marshallTo: function (dataView, offset) {
+                    dataView.setInt32(offset, this.value, connection.littleEndian);
+                }
+            },
+            {
+                value: errorMsg,
+                type: "s",
+                size: 4 + errorMsg.length,
+                optional: false,
+                marshallTo: function (dataView, offset) {
+                    dataView.setInt32(offset, 4, connection.littleEndian);
+                    let writeOffset = offset + 4;
+                    this.value.forEach(new function (char) {
+                        dataView.setUint8(writeOffset, char);
+                        writeOffset += 1;
+                    });
+                }
+            }
+        ]);
     };
 
     //--constructor--
     const connection = connection;
 };
 
-//WIRE FORMAT proposal:
-
-//example message format from client to server:
-//corresponding wire message: 00 03 (id) 01 (opcode) 6e 3f 69 6f 61 00 (signature) 00 07 (arg0) 00 00 00 00 (arg1) 00 05 (arg2) 00 00 00 14 fe 12 74 54 67 43 fa a8 ea b3 c8 93 75 36 8e 72 75 92 74 81 (arg3)
-// message = {
-//     id: 3, //used to find the object with id 3: uint16
-//     opcode: 1, //used find the function of object that corresponds to opcode 1. Opcode 0 is reserved: uint8
-//     signature: "n?ioa", //used to define the type of the arguments (n= new object with id: uin16, ? = optional, i = integer: sint32, o = existing object with id: uin16, a = array): null terminated char array
-//     argsArray: [7, undefined, 5, new ArrayBuffer(20)] // to be used with js' "apply" call.
-// };
-
-//example generated object:
-// wf.Foo = function (connection) {
-//
-//     this.1 = function (argsArray) {
-//         this.bar.apply(this, argsArray);
-//     };
-//
-//     //--constructor--
-//     wf._Object.call(this, connection);
-// };
-// wf.Foo.prototype = Object.create(wf._Object);
-
 wf.Connection = function (webSocket) {
-
-    //--private properties--
+    //--public properties--
     this.objects = new Map();
+    this.littleEndian = true;
 
     //--private functions--
     const unmarshall = function (blob) {
-        //TODO convert blob to message;
+        //example wire message
+        //00 03 01 6e 00 07 69 00 00 04 00 61 00 00 00 03 ef fa 7e
+        //translates to:
+        //3 (=id),1 (=opcode),n (=new object id) 7 (id value), i (integer) 1024 (integer value), a (array) 3 (array size) [0xef 0xfa 0x7e] (array value)
 
+        //TODO convert blob to message;
     };
 
     const dispatch = function (message) {
@@ -59,6 +65,7 @@ wf.Connection = function (webSocket) {
 
     const onSocketOpen = function (event) {
         //TODO send back-end minimal required browser info (we start with screen size)
+        //TODO the first request shall be a json informing the host of our properties.
     };
 
     const onSocketClose = function (event) {
@@ -70,6 +77,9 @@ wf.Connection = function (webSocket) {
     };
 
     const onSocketMessage = function (event) {
+        //TODO as a first response we expect the hosts' wire message endianess
+        //TODO the first response shall be a json informing us of the host's properties.
+
         const message = unmarshall(event);
         dispatch(message);
     };
@@ -84,133 +94,39 @@ wf.Connection = function (webSocket) {
     };
 
     //--public functions--
-    this.marshall = function (id, opcode, signature, argsArray) {
-
-        //TODO make this function great again.
-
-        //determine length
-        //id(2)+opcode(1)
-        var size = 3;
-        //signature.length+null byte
-        size += signature.length + 1;
-
-        const wireCharsAscii = [];
-        var argi = 0;
-        //first pass, determine size
-        var optional = false;
-        for (var i = 0; i < signature.length; i++) {
-            var signatureChar = signature[i];
-            wireCharsAscii[i] = signatureChar.charCodeAt(i);
-            switch (signatureChar) {
-                case "i": //integer, sint32 -> 4
-                case "f": //fixed, uint32 -> 4
-                    size += 4;
-                    argi++;
-                    break;
-                case "o": //object, uint16 -> 2
-                case "n": //new object, uint16 -> 2
-                    size += 2;
-                    argi++;
-                    break;
-                case "s": //string, 4+data length (in bytes)
-                case "a": //array, 4+data length (in bytes)
-
-                    const arg = argsArray[argi];
-                    if (optional && arg == undefined) {
-                        size += 4;
-                    }
-                    else if (arg instanceof ArrayBuffer || arg instanceof String) {
-                        size += 4;
-                        size += arg.length;
-                    }
-                    else {
-                        throw new TypeError("Argument #" + argi + " is not of type ArrayBuffer or String.");
-                    }
-
-                    argi++;
-                    break;
-                case "?": //optional -> 0
-                    optional = true;
-                    break;
-                default:
-                //TODO more types
+    this.marshall = function (id, opcode, argsArray) {
+        //determine required wire message length
+        let size = 2 + 1;  //id+opcode
+        argsArray.forEach(function (arg) {
+            if (arg.optional) {
+                size += 1; //add one for the optional (=?) ascii char
             }
-        }
-        wireCharsAscii[wireCharsAscii.length] = 0;
-
-        const wireMsg = new ArrayBuffer(size);
-        const wireMsgView = new DataView(wireMsg);
-        var offset = 0;
-
-        wireMsgView.setInt16(offset, id, true);//id
-        offset += 2;
-
-        wireMsgView.setInt8(offset, opcode);//opcode
-        offset += 1;
-
-        wireCharsAscii.forEach(function (asciiChar) {
-            wireMsg.setInt8(offset, asciiChar);
-            offset += 1;
+            size += 1; //add one for the ascii char
+            size += arg.size; //add size of the actual argument values
         });
 
-        //second pass, fill in buffer
-        argi = 0;
-        optional = false;
-        for (i = 0; i < signature.length; i++) {
-            signatureChar = signature[i];
-            const arg = argsArray[argi];
-            switch (signatureChar) {
-                case "i":
-                case "f":
-                    if (optional && arg == undefined) {
-                        wireMsgView.setInt32(offset, 0, true);
-                        optional = false;
-                    } else {
-                        wireMsgView.setInt32(offset, arg, true);
-                    }
-                    offset += 4;
+        const wireMsgBuffer = new ArrayBuffer(size);
+        const wireMsgView = new DataView(wireMsgBuffer);
+        let offset = 0;
 
-                    argi++;
-                    break;
-                case "o":
-                case "n":
-                    if (optional && arg == undefined) {
-                        wireMsgView.setUint16(offset, 0, true);
-                        optional = false;
-                    } else {
-                        wireMsgView.setUint16(offset, arg, true);
-                    }
-                    offset += 2;
+        //write actual wire message
+        wireMsgView.setUint16(offset, id, this.littleEndian);//id
+        offset += 2;
+        wireMsgView.setUint8(offset, opcode);//opcode
+        offset += 1;
 
-                    argi++;
-                    break;
-                case "s":
-                case "a":
-                    if (optional && arg == undefined) {
-                        wireMsgView.setInt32(offset, 0, true);
-                        offset += 4;
-                        optional = false;
-                    } else {
-                        wireMsgView.setInt32(offset, arg.length, true);
-                        offset += 4;
-                        arg.forEach(new function (byte) {
-                            wireMsg.setInt8(offset, byte);
-                            offset += 1;
-                        });
-                    }
-
-                    argi++;
-                    break;
-                case "?":
-                    optional = true;
-                    break;
-                default:
-                    throw new TypeError("Unrecognized type " + signatureChar + " in signature for arg #" + argi);
-                //TODO more types
+        argsArray.forEach(function (arg) {
+            if (arg.optional) {
+                wireMsgView.setUint8(offset, "?".codePointAt(0)); //optional ascii char
+                offset += 1;
             }
-        }
+            wireMsgView.setUint8(offset, arg.type.codePointAt(0)); //argument type ascii char
+            offset += 1;
+            arg.marshallTo(wireMsgView, offset); //write actual argument value to buffer
+            offset += arg.size;
+        });
 
-        socket.send(wireMsg);
+        socket.send(wireMsgBuffer);
     };
 
     this.close = function () {
@@ -222,8 +138,8 @@ wf.Connection = function (webSocket) {
 
     this._registerObject = function (object) {
         //find unused id.
-        var id = 1;
-        while (this._objects.has(id)) {
+        let id = 1;
+        while (this.objects.has(id)) {
             id++;
         }
         object._id = id;
