c2d17b83315d6959a256b14ef137ec3b133624ec
Erik De Rijcke <derijcke.erik@gmail.com>
2016-11-24 16:31:27 +0100

    experiment with unmarshall function

--- app/westfield-core.js
+++ app/westfield-core.js
@@ -3,57 +3,387 @@
 //define westfield namespace as wf
 const wf = wf || {};
 
+//-- js argument to wire format literals: integer (number), float (number), object (wf._Object), new object (wf._Object), string (string), array (ArrayBuffer) --
 /**
  *
- * @param {Number} number
- * @constructor
+ * @param {Number} arg
+ * @returns {{value: number, type: string, size: number, optional: boolean, marshallTo: marshallTo}}
+ * @private
  */
-wf.Fixed = function (number) {
-    //TODO
+wf._int = function (arg) {
+    return {
+        value: arg,
+        type: "i",
+        size: 4,
+        optional: false,
+        /**
+         *
+         * @param {DataView} dataView
+         * @param {Number} offset
+         */
+        marshallTo: function (dataView, offset) {
+            dataView.setInt32(offset, this.value);
+        }
+    };
 };
 
+/**
+ *
+ * @param {Number} arg
+ * @returns {{value: number, type: string, size: number, optional: boolean, marshallTo: marshallTo}}
+ * @private
+ */
+wf._intOptional = function (arg) {
+    return {
+        value: arg,
+        type: "i",
+        size: 4,
+        optional: true,
+        /**
+         *
+         * @param {DataView} dataView
+         * @param {Number} offset
+         */
+        marshallTo: function (dataView, offset) {
+            if (arg == null) {
+                dataView.setInt32(offset, 0);
+            } else {
+                dataView.setInt32(offset, this.value);
+            }
+        }
+    }
+};
+
+/**
+ *
+ * @param {Number} arg
+ * @returns {{value: number, type: string, size: number, optional: boolean, marshallTo: marshallTo}}
+ * @private
+ */
+wf._float = function (arg) {
+    return {
+        value: arg,
+        type: "f",
+        size: 4,
+        optional: false,
+        /**
+         *
+         * @param {DataView} dataView
+         * @param {Number} offset
+         */
+        marshallTo: function (dataView, offset) {
+            dataView.setFloat32(offset, this.value);
+        }
+    };
+};
+
+/**
+ *
+ * @param {Number} arg
+ * @returns {{value: number, type: string, size: number, optional: boolean, marshallTo: marshallTo}}
+ * @private
+ */
+wf._floatOptional = function (arg) {
+    return {
+        value: arg,
+        type: "f",
+        size: 4,
+        optional: true,
+        /**
+         *
+         * @param {DataView} dataView
+         * @param {Number} offset
+         */
+        marshallTo: function (dataView, offset) {
+            if (arg == null) {
+                dataView.setFloat32(offset, 0);
+            }
+            else {
+                dataView.setFloat32(offset, this.value);
+            }
+        }
+    };
+};
+
+/**
+ *
+ * @param {wf._Object} arg
+ * @returns {{value: wf._Object, type: string, size: number, optional: boolean, marshallTo: marshallTo}}
+ * @private
+ */
+wf._object = function (arg) {
+    return {
+        value: arg,
+        type: "o",
+        size: 2,
+        optional: false,
+        /**
+         *
+         * @param {DataView} dataView
+         * @param {Number} offset
+         */
+        marshallTo: function (dataView, offset) {
+            dataView.setUint16(offset, this.value._id);
+        }
+    };
+};
+
+/**
+ *
+ * @param {wf._Object} arg
+ * @returns {{value: wf._Object, type: string, size: number, optional: boolean, marshallTo: marshallTo}}
+ * @private
+ */
+wf._objectOptional = function (arg) {
+    return {
+        value: arg,
+        type: "o",
+        size: 2,
+        optional: true,
+        /**
+         *
+         * @param {DataView} dataView
+         * @param {Number} offset
+         */
+        marshallTo: function (dataView, offset) {
+            if (arg == null) {
+                dataView.setUint16(offset, 0);
+            } else {
+                dataView.setUint16(offset, this.value._id);
+            }
+        }
+    };
+};
+
+/**
+ *
+ * @param {wf._Object} arg
+ * @returns {{value: wf._Object, type: string, size: number, optional: boolean, marshallTo: marshallTo}}
+ * @private
+ */
+wf._newObject = function (arg) {
+    return {
+        value: arg,
+        type: "n",
+        size: 2 + 1 + (typeof arg).length,//id+length+name
+        optional: false,
+        /**
+         *
+         * @param {DataView} dataView
+         * @param {Number} offset
+         */
+        marshallTo: function (dataView, offset) {
+            dataView.setUint16(offset, this.value._id);
+            let writeOffset = offset + 2;
+            const objType = (typeof this.value);
+            dataView.setUint8(writeOffset, objType.length);
+            writeOffset += 1;
+            objType.forEach(new function (char) {
+                dataView.setUint8(writeOffset, char.codePointAt(0));
+                writeOffset += 1;
+            });
+        }
+    };
+};
+
+/**
+ *
+ * @param {wf._Object} arg
+ * @returns {{value: wf._Object, type: string, size: number, optional: boolean, marshallTo: marshallTo}}
+ * @private
+ */
+wf._newObjectOptional = function (arg) {
+    return {
+        value: arg,
+        type: "n",
+        size: 2 + (function () {
+            if (arg == null) {
+                return 0;
+            } else {
+                return 1 + (typeof arg).length;
+            }
+        })(),//id+length+name
+        optional: true,
+        /**
+         *
+         * @param {DataView} dataView
+         * @param {Number} offset
+         */
+        marshallTo: function (dataView, offset) {
+            if (this.value == null) {
+                dataView.setUint16(offset, 0);
+            } else {
+                dataView.setUint16(offset, this.value._id);
+                let writeOffset = offset + 2;
+                const objType = (typeof this.value);
+                dataView.setUint8(writeOffset, objType.length);
+                writeOffset += 1;
+                objType.forEach(new function (char) {
+                    dataView.setUint8(writeOffset, char.codePointAt(0));
+                    writeOffset += 1;
+                });
+            }
+        }
+    };
+};
+
+/**
+ *
+ * @param {String} arg
+ * @returns {{value: String, type: string, size: number, optional: boolean, marshallTo: marshallTo}}
+ * @private
+ */
+wf._string = function (arg) {
+    return {
+        value: arg,
+        type: "s",
+        size: 4 + arg.length,
+        optional: false,
+        /**
+         *
+         * @param {DataView} dataView
+         * @param {Number} offset
+         */
+        marshallTo: function (dataView, offset) {
+            dataView.setInt32(offset, this.value.length);
+            let writeOffset = offset + 4;
+            this.value.forEach(new function (char) {
+                dataView.setUint8(writeOffset, char.codePointAt(0));
+                writeOffset += 1;
+            });
+        }
+    };
+};
+
+/**
+ *
+ * @param {String} arg
+ * @returns {{value: String, type: string, size: number, optional: boolean, marshallTo: marshallTo}}
+ * @private
+ */
+wf._stringOptional = function (arg) {
+    return {
+        value: arg,
+        type: "s",
+        size: 4 + (function () {
+            if (arg == null) {
+                return 0;
+            } else {
+                return arg.length;
+            }
+        })(),
+        optional: false,
+        /**
+         *
+         * @param {DataView} dataView
+         * @param {Number} offset
+         */
+        marshallTo: function (dataView, offset) {
+            if (this.value == null) {
+                dataView.setInt32(offset, 0);
+            } else {
+                dataView.setInt32(offset, this.value.length);
+                let writeOffset = offset + 4;
+                this.value.forEach(new function (char) {
+                    dataView.setUint8(writeOffset, char.codePointAt(0));
+                    writeOffset += 1;
+                });
+            }
+        }
+    };
+};
+
+/**
+ *
+ * @param {TypedArray} arg
+ * @returns {{value: TypedArray, type: string, size: number, optional: boolean, marshallTo: marshallTo}}
+ * @private
+ */
+wf._array = function (arg) {
+    return {
+        value: arg,
+        type: "a",
+        size: 4 + arg.buffer.byteLength,
+        optional: false,
+        /**
+         *
+         * @param {DataView} dataView
+         * @param {Number} offset
+         */
+        marshallTo: function (dataView, offset) {
+            dataView.setInt32(offset, this.value.buffer.byteLength);
+            let writeOffset = offset + 4;
+            new Uint8Array(this.value.buffer, 0, this.value.buffer.byteLength).forEach(new function (byte) {
+                dataView.setUint8(writeOffset, byte);
+                writeOffset += 1;
+            });
+        }
+    };
+};
+
+/**
+ *
+ * @param {TypedArray} arg
+ * @returns {{value: ArrayBuffer, type: string, size: number, optional: boolean, marshallTo: marshallTo}}
+ * @private
+ */
+wf._arrayOptional = function (arg) {
+    return {
+        value: arg,
+        type: "a",
+        size: 4 + (function () {
+            if (arg == null) {
+                return 0;
+            } else {
+                return arg.buffer.byteLength;
+            }
+        })(),
+        optional: true,
+        /**
+         *
+         * @param {DataView} dataView
+         * @param {Number} offset
+         */
+        marshallTo: function (dataView, offset) {
+            if (this.value == null) {
+                dataView.setInt32(offset, 0);
+            } else {
+                dataView.setInt32(offset, this.value.buffer.byteLength);
+                let writeOffset = offset + 4;
+                new Uint8Array(this.value.buffer, 0, this.value.buffer.byteLength).forEach(new function (byte) {
+                    dataView.setUint8(writeOffset, byte);
+                    writeOffset += 1;
+                });
+            }
+        }
+    };
+};
+
+/**
+ *
+ * @param {wf.Connection} connection
+ * @private
+ */
 wf._Object = function (connection) {
 
-    //--public functions--
+    //--functions--
     /**
-     *
+     * Deletes this object from the local pool of objects and notify the remote about this object's deletion.
      */
     this.delete = function () {
         this._connection._objects.remove(this._id);
         this._connection._marshall(this._id, 0, []);//opcode 0 is reserved for deletion
+        delete this._id;
     };
 
     /**
+     * Post an error to the remote.
      *
      * @param {Number} errorCode an integer error code
      * @param {String} errorMsg the error message
      */
     this.postError = function (errorCode, errorMsg) {
-        this._connection._marshall(this._id, 255, [//opcode 255 is reserved for error
-            {
-                value: errorCode,
-                type: "i",
-                size: 4,
-                optional: false,
-                marshallTo: function (dataView, offset) {
-                    dataView.setInt32(offset, this.value, this._connection._littleEndian);
-                }
-            },
-            {
-                value: errorMsg,
-                type: "s",
-                size: 4 + errorMsg.length,
-                optional: false,
-                marshallTo: function (dataView, offset) {
-                    dataView.setInt32(offset, 4, this._connection._littleEndian);
-                    let writeOffset = offset + 4;
-                    this.value.forEach(new function (char) {
-                        dataView.setUint8(writeOffset, char);
-                        writeOffset += 1;
-                    });
-                }
-            }
-        ]);
+        this._connection._marshall(this._id, 255, [wf._int(errorCode), wf._string(errorMsg)]);//opcode 255 is reserved for error
     };
 
     //--constructor--
@@ -64,17 +394,14 @@ wf.Connection = function (socketUrl) {
 
     //--properties--
     /**
+     * Pool of objects that live on this connection.
+     * Key: Number, Value: a subtype of wf._Object
+     * wf._Object._id == Key
      *
      * @type {Map}
      * @private
      */
     this._objects = new Map();
-    /**
-     *
-     * @type {boolean}
-     * @private
-     */
-    this._littleEndian = true;
 
     //--functions--
     /**
@@ -84,47 +411,82 @@ wf.Connection = function (socketUrl) {
      * @private
      */
     this._unmarshallArg = function (wireMsg) {
-        const typeAscii = wireMsg.getUint8(wireMsg.offset);
+        let typeAscii = wireMsg.getUint8(wireMsg.offset);
         wireMsg.offset += 1;
 
+        const optional = String.fromCharCode(typeAscii) == "?";
+        if (optional) {
+            typeAscii = wireMsg.getUint8(wireMsg.offset);
+            wireMsg.offset += 1;
+        }
+
+        let arg = null;
         switch (String.fromCharCode(typeAscii)) {
-            case "i"://{Number}
-                const arg = wireMsg.getInt32(wireMsg.offset, this._littleEndian);
+            case "i"://integer {Number}
+                arg = wireMsg.getInt32(wireMsg.offset);
                 wireMsg.offset += 4;
-                return arg;
-            case "f"://{wf.Fixed}
-                const arg = wireMsg.getInt32(wireMsg.offset, this._littleEndian);
+                break;
+            case "f"://float {Number}
+                arg = wireMsg.getFloat32(wireMsg.offset);
                 wireMsg.offset += 4;
-                return new wf.Fixed(arg);
+                break;
             case "o"://existing object, subtype of {wf._Object}
-                const id = wireMsg.getUint16(wireMsg.offset, this._littleEndian);
+                const id = wireMsg.getUint16(wireMsg.offset);
                 wireMsg.offset += 2;
-                return this._objects.get(id);
+                if (optional && id == 0) {
+                    arg = null;
+                } else {
+                    arg = this._objects.get(id);
+                }
+                break;
             case "n":///new object, subtype of {wf._Object}
-                const id = wireMsg.getUint16(wireMsg.offset, this._littleEndian);
+                const id = wireMsg.getUint16(wireMsg.offset);
                 wireMsg.offset += 2;
-                const typeNameSize = wireMsg.getUint8(wireMsg.offset);
-                wireMsg.offset += 1;
-                const byteArray = new Uint8Array(wireMsg.buffer, wireMsg.offset, typeNameSize);
-                wireMsg.offset += typeNameSize;
+                if (optional && id == 0) {
+                    arg = null;
+                } else {
+                    const typeNameSize = wireMsg.getUint8(wireMsg.offset);
+                    wireMsg.offset += 1;
+                    const byteArray = new Uint8Array(wireMsg.buffer, wireMsg.offset, typeNameSize);
+                    wireMsg.offset += typeNameSize;
 
-                const type = String.fromCharCode.apply(null, byteArray);
-                const object = new wf[type](this);
-                object._id = id;
-                this._objects.set(object._id, object);
-
-                return object;
+                    const type = String.fromCharCode.apply(null, byteArray);
+                    const newObject = new wf[type](this);
+                    newObject._id = id;
+                    this._objects.set(newObject._id, newObject);
+                    arg = newObject;
+                }
+                break;
             case "s"://{String}
-                const stringSize = wireMsg.getInt32(wireMsg.offset, this._littleEndian);
+                const stringSize = wireMsg.getInt32(wireMsg.offset);
                 wireMsg.offset += 4;
-                const byteArray = new Uint8Array(wireMsg.buffer, wireMsg.offset, stringSize);
-                wireMsg.offset += stringSize;
-
-                return String.fromCharCode.apply(null, byteArray);
-            case "a"://{ArrayBuffer}
-                //TODO
+                if (optional && stringSize == 0) {
+                    arg = null;
+                }
+                else {
+                    const byteArray = new Uint8Array(wireMsg.buffer, wireMsg.offset, stringSize);
+                    wireMsg.offset += stringSize;
+                    arg = String.fromCharCode.apply(null, byteArray);
+                }
+                break;
+            case "a"://{Uint8Array}
+                const arraySize = wireMsg.getInt32(wireMsg.offset);
+                wireMsg.offset += 4;
+                if (optional && arraySize == 0) {
+                    arg = null;
+                } else {
+                    const byteArray = new Uint8Array(wireMsg.buffer, wireMsg.offset, arraySize);
+                    wireMsg.offset += arraySize;
+                    arg = byteArray;
+                }
                 break;
         }
+
+        if (arg == null) {
+            //TODO throw unmarshalling exception
+        }
+
+        return arg;
     };
 
     /**
@@ -143,7 +505,7 @@ wf.Connection = function (socketUrl) {
         const wireMsg = new DataView(message);
         wireMsg.offset = 0;
 
-        const id = wireMsg.getUint16(wireMsg.offset, this._littleEndian);
+        const id = wireMsg.getUint16(wireMsg.offset);
         wireMsg.offset += 2;
 
         const opcode = wireMsg.getUint8(wireMsg.offset);
@@ -168,6 +530,7 @@ wf.Connection = function (socketUrl) {
     this._onSocketOpen = function (event) {
         //TODO send back-end minimal required browser info (we start with screen size)
         //TODO the first request shall be a json informing the host of our properties.
+        //all subsequent message will be in the binary wire format.
     };
 
     this._onSocketClose = function (event) {
@@ -179,10 +542,8 @@ wf.Connection = function (socketUrl) {
     };
 
     this._onSocketMessage = function (event) {
-        //TODO as a first response we expect the hosts' wire message endianess
         //TODO the first response shall be a json informing us of the host's properties.
         //all subsequent message will be in the binary wire format.
-
         const message = this._unmarshall(event);
         message.obj[message.opcode](message.args);
     };
@@ -220,7 +581,7 @@ wf.Connection = function (socketUrl) {
         let offset = 0;
 
         //write actual wire message
-        wireMsgView.setUint16(offset, id, this._littleEndian);//id
+        wireMsgView.setUint16(offset, id);//id
         offset += 2;
         wireMsgView.setUint8(offset, opcode);//opcode
         offset += 1;
