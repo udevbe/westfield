d1fa71b7193a5dc42fd6f47561a2757f2a85733b
Erik De Rijcke <derijcke.erik@gmail.com>
2016-11-29 10:30:56 +0100

    don't implement server side core js for now

--- app/westfield-server-core.js
+++
@@ -1,9 +0,0 @@
-//TODO figure out how to setup a websocket server (use express?)
-
-const WebSocketServer = require('ws').Server;
-
-//westfield server namespace
-const wfs = wfs || {};
-
-const wss = new WebSocketServer({port: 8080});
-
