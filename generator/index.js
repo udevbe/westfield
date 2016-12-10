0a3ccf76c1466f15bc565487cac158f38b29e9c1
Erik De Rijcke <derijcke.erik@gmail.com>
2016-12-10 23:37:07 +0100

    generate empty protocol interface classes

--- generator/index.js
+++ generator/index.js
@@ -3,33 +3,36 @@ const util = require('util');
 
 const xml2js = require('xml2js');
 
-//TODO parse wayland xml & output js that makes use of core library
 const wfg = {};
 
 wfg.ProtocolParser = class {
 
-    _parseInterface(protocolItf) {
+    _parseInterface(out, protocolItf) {
         const itfName = protocolItf.$.name;
         const itfVersion = protocolItf.$.version;
         const itfDesc = protocolItf.description[0]._;
         const itfSummary = protocolItf.description[0].$.summary;
 
         console.log(util.format("Processing interface %s v%d", itfName, itfVersion));
+
+        //if version is 1, we extends from WObject, else we extend from the previous version.
+        out.write(util.format("wfc.%sV%d = class %s extends wfc.WObject {\n", itfName, itfVersion, itfName));
+        out.write("};\n");
     }
 
     _parseProtocol(jsonProtocol) {
         const protocolName = jsonProtocol.protocol.$.name;
-        this._out = fs.createWriteStream(util.format("westfield-client-%s.js", protocolName));
-        this._out.on('open', (fd) => {
+        const out = fs.createWriteStream(util.format("westfield-client-%s.js", protocolName));
+        out.on('open', (fd) => {
             jsonProtocol.protocol.copyright.forEach((val) => {
                 val.split("\n").forEach((line) => {
-                    this._out.write("//" + line + "\n");
+                    out.write("//" + line + "\n");
                 });
             });
 
-            //TODO namespace object
-
-            jsonProtocol.protocol.interface.forEach(this._parseInterface);
+            jsonProtocol.protocol.interface.forEach((itf) => {
+                this._parseInterface(out, itf);
+            });
         });
     }
 
