3867d8fd0e86f81d51eac279c09a1b93d255b9a9
Erik De Rijcke <derijcke.erik@gmail.com>
2016-12-12 13:14:39 +0100

    generate even glue functions

--- generator/index.js
+++ generator/index.js
@@ -11,7 +11,7 @@ wfg.ProtocolParser = class {
     ["uint"](argName, optional) {
         return {
             jsType: optional ? "?Number" : "Number",
-            marshall: optional ? util.format("wfc._intOptional(%s)", argName) : util.format("wfc._int(%s)", argName)
+            marshallGen: optional ? util.format("wfc._intOptional(%s)", argName) : util.format("wfc._int(%s)", argName)
         };
     }
 
@@ -19,7 +19,7 @@ wfg.ProtocolParser = class {
     ["fixed"](argName, optional) {
         return {
             jsType: optional ? "?Number" : "Number",
-            marshall: optional ? util.format("wfc._floatOptional(%s)", argName) : util.format("wfc._float(%s)", argName)
+            marshallGen: optional ? util.format("wfc._floatOptional(%s)", argName) : util.format("wfc._float(%s)", argName)
         };
     }
 
@@ -27,7 +27,7 @@ wfg.ProtocolParser = class {
     ["fd"](argName, optional) {
         return {
             jsType: optional ? "?Number" : "Number",
-            marshall: optional ? util.format("wfc._intOptional(%s)", argName) : util.format("wfc._int(%s)", argName)
+            marshallGen: optional ? util.format("wfc._intOptional(%s)", argName) : util.format("wfc._int(%s)", argName)
         };
     }
 
@@ -35,53 +35,125 @@ wfg.ProtocolParser = class {
     ["int"](argName, optional) {
         return {
             jsType: optional ? "?Number" : "Number",
-            marshall: optional ? util.format("wfc._intOptional(%s)", argName) : util.format("wfc._int(%s)", argName)
+            marshallGen: optional ? util.format("wfc._intOptional(%s)", argName) : util.format("wfc._int(%s)", argName)
         };
     }
 
     ["float"](argName, optional) {
         return {
             jsType: optional ? "?Number" : "Number",
-            marshall: optional ? util.format("wfc._floatOptional(%s)", argName) : util.format("wfc._float(%s)", argName)
+            marshallGen: optional ? util.format("wfc._floatOptional(%s)", argName) : util.format("wfc._float(%s)", argName)
         };
     }
 
     ["double"](argName, optional) {
         return {
             jsType: optional ? "?Number" : "Number",
-            marshall: optional ? util.format("wfc._doubleOptional(%s)", argName) : util.format("wfc._double(%s)", argName)
+            marshallGen: optional ? util.format("wfc._doubleOptional(%s)", argName) : util.format("wfc._double(%s)", argName)
         };
     }
 
     ["object"](argName, optional) {
         return {
             jsType: optional ? "?*" : "*",
-            marshall: optional ? util.format("wfc._objectOptional(%s)", argName) : util.format("wfc._object(%s)", argName)
+            marshallGen: optional ? util.format("wfc._objectOptional(%s)", argName) : util.format("wfc._object(%s)", argName)
         };
     }
 
     ["new_id"](argName, optional) {
         return {
             jsType: optional ? "?*" : "*",
-            marshall: optional ? util.format("wfc._newObjectOptional(%s)", argName) : util.format("wfc._newObject(%s)", argName)
+            marshallGen: optional ? util.format("wfc._newObjectOptional(%s)", argName) : util.format("wfc._newObject(%s)", argName)
         };
     }
 
     ["string"](argName, optional) {
         return {
             jsType: optional ? "?string" : "string",
-            marshall: optional ? util.format("wfc._stringOptional(%s)", argName) : util.format("wfc._string(%s)", argName)
+            marshallGen: optional ? util.format("wfc._stringOptional(%s)", argName) : util.format("wfc._string(%s)", argName)
         };
     }
 
     ["array"](argName, optional) {
         return {
             jsType: optional ? "?ArrayBuffer" : "ArrayBuffer",
-            marshall: optional ? util.format("wfc._arrayOptional(%s)", argName) : util.format("wfc._array(%s)", argName)
+            marshallGen: optional ? util.format("wfc._arrayOptional(%s)", argName) : util.format("wfc._array(%s)", argName)
         };
     }
 
+    _generateArgs(out, evReq) {
+        if (evReq.hasOwnProperty("arg")) {
+            const evArgs = evReq.arg;
+            for (let i = 0; i < evArgs.length; i++) {
+                const arg = evArgs[i];
+                const argName = arg.$.name;
+                if (i !== 0) {
+                    out.write(", ");
+                }
+                out.write(argName);
+            }
+        }
+    }
+
+    _generateIfEventGlue(out, itfEvent, opcode) {
+
+        const evName = itfEvent.$.name;
+
+        out.write(util.format("\t[%d](", opcode));
+        this._generateArgs(out, itfEvent);
+        out.write("){\n");
+        out.write(util.format("\t\tthis._iface.%s(", evName));
+        this._generateArgs(out, itfEvent);
+        out.write(");\n");
+        out.write("\t}\n\n");
+    }
+
+    _parseItfEvent(out, itfEvent) {
+        const sinceVersion = itfEvent.$.hasOwnProperty("since") ? parseInt(itfEvent.$.since) : 1;
+        const evName = itfEvent.$.name;
+
+        //function docs
+        const description = itfEvent.description;
+        description.forEach((val) => {
+            out.write("\n\t\t\t/**\n");
+            if (val.hasOwnProperty("_")) {
+                val._.split("\n").forEach((line) => {
+                    out.write("\t\t\t *" + line + "\n");
+                });
+            }
+
+            if (itfEvent.hasOwnProperty("arg")) {
+                const evArgs = itfEvent.arg;
+                out.write("\t\t\t *\n");
+                evArgs.forEach((arg) => {
+                    const argDescription = arg.$.summary;
+                    const argName = arg.$.name;
+                    const argType = arg.$.type;
+
+                    const allowNull = arg.$.hasOwnProperty("allow-null") && (arg.$["allow-null"] === "true");
+                    out.write(util.format("\t\t\t * @param {%s} %s %s \n", this[argType](argName, allowNull).jsType, argName, argDescription));
+                });
+                out.write("\t\t\t *\n");
+
+            }
+            out.write(util.format("\t\t\t * @since %d\n", sinceVersion));
+            out.write("\t\t\t *\n");
+            out.write("\t\t\t */\n");
+        });
+
+        //function
+        out.write(util.format("\t\t\t%s(", evName));
+        this._generateArgs(out, itfEvent);
+        out.write(") {},\n");
+    }
+
     _parseItfRequest(out, itfRequest, opcode, itfVersion) {
+
+        const sinceVersion = itfRequest.$.hasOwnProperty("since") ? parseInt(itfRequest.$.since) : 1;
+        if (sinceVersion !== itfVersion) {
+            return;
+        }
+
         const reqName = itfRequest.$.name;
 
         //function docs
@@ -102,28 +174,20 @@ wfg.ProtocolParser = class {
                     const argName = arg.$.name;
                     const argType = arg.$.type;
 
-                    //TODO process if arg is optional
-                    out.write(util.format("\t * @param {%s} %s %s \n", this[argType](argName, false).jsType, argName, argDescription));
+                    const allowNull = arg.$.hasOwnProperty("allow-null") && (arg.$["allow-null"] === "true");
+                    out.write(util.format("\t * @param {%s} %s %s \n", this[argType](argName, allowNull).jsType, argName, argDescription));
                 });
                 out.write("\t *\n");
-            }
 
+            }
+            out.write(util.format("\t * @since %d\n", sinceVersion));
+            out.write("\t *\n");
             out.write("\t */\n");
         });
 
         //function
         out.write(util.format("\t%s(", reqName));
-        if (itfRequest.hasOwnProperty("arg")) {
-            const reqArgs = itfRequest.arg;
-            for (let i = 0; i < reqArgs.length; i++) {
-                const arg = reqArgs[i];
-                const argName = arg.$.name;
-                if (i !== 0) {
-                    out.write(", ");
-                }
-                out.write(argName);
-            }
-        }
+        this._generateArgs(out, itfRequest);
         out.write(") {\n");
         out.write(util.format("\t\tthis._connection._marshall(this._id, %d, [", opcode));
 
@@ -140,8 +204,8 @@ wfg.ProtocolParser = class {
                     out.write(", ");
                 }
 
-                //TODO process if arg is optional
-                out.write(this[argType](argName, false).marshall);
+                const allowNull = arg.$.hasOwnProperty("allow-null") && (arg.$["allow-null"] === "true");
+                out.write(this[argType](argName, allowNull).marshallGen);
             }
         }
 
@@ -155,7 +219,6 @@ wfg.ProtocolParser = class {
 
         console.log(util.format("Processing interface %s v%d", itfName, itfVersion));
 
-        //if version is 1, we extends from WObject, else we extend from the previous version.
         for (let i = 1; i <= itfVersion; i++) {
 
             //class docs
@@ -182,13 +245,35 @@ wfg.ProtocolParser = class {
                 }
             }
 
-            //interface can have no requests, so we need to check for it's absence.
+            //requests
             if (protocolItf.hasOwnProperty("request")) {
                 const itfRequests = protocolItf.request;
                 for (let j = 0; j < itfRequests.length; j++) {
                     this._parseItfRequest(out, itfRequests[j], j + 1, i);
                 }
             }
+
+            //constructor
+            out.write("\n\tconstructor(connection) {\n");
+            out.write("\t\tsuper(connection, {\n");
+            out.write(util.format("\t\t\tname: \"%s\",\n", itfName));
+            //events
+            if (protocolItf.hasOwnProperty("event")) {
+                protocolItf.event.forEach((event) => {
+                    this._parseItfEvent(out, event);
+                });
+            }
+            out.write("\t\t});\n");
+            out.write("\t}\n\n");
+
+            //glue event functions
+            if (protocolItf.hasOwnProperty("event")) {
+                const itfEvents = protocolItf.event;
+                for (let j = 0; j < itfEvents.length; j++) {
+                    this._generateIfEventGlue(out, itfEvents[j], j + 1);
+                }
+            }
+
 
             out.write("};\n");
         }
@@ -209,6 +294,8 @@ wfg.ProtocolParser = class {
             jsonProtocol.protocol.interface.forEach((itf) => {
                 this._parseInterface(out, itf);
             });
+
+            console.log('Done');
         });
     }
 
@@ -222,7 +309,6 @@ wfg.ProtocolParser = class {
                 //console.log(util.inspect(result, false, null));
 
                 this._parseProtocol(result);
-                console.log('Done');
             });
         });
     }
