83433a133a25f9863d1d1158becca0f7b2bcf8f7
Erik De Rijcke <derijcke.erik@gmail.com>
2017-01-04 16:28:57 +0100

    fix unit tests

--- server/java/westfield.server/westfield.server.runtime/src/test/java/org/freedesktop/westfield/server/WArgsTest.java
+++ server/java/westfield.server/westfield.server.runtime/src/test/java/org/freedesktop/westfield/server/WArgsTest.java
@@ -3,17 +3,17 @@ package org.freedesktop.westfield.server;
 import com.google.common.truth.Truth;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.nio.IntBuffer;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 
 import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -27,6 +27,8 @@ public class WArgsTest {
         //given
         final int resourceId = 123;
         when(this.wResource.getId()).thenReturn(resourceId);
+        final WClient client = mock(WClient.class);
+        when(this.wResource.getClient()).thenReturn(client);
         final int opcode = 456;
         final WArgs wArgs = new WArgs(this.wResource,
                                       opcode);
@@ -45,9 +47,12 @@ public class WArgsTest {
              .arg(fixedArg);
 
         //when
-        final ByteBuffer wireMessage = wArgs.toWireMessage();
+        wArgs.send();
 
         //then
+        final ArgumentCaptor<ByteBuffer> argumentCaptor = ArgumentCaptor.forClass(ByteBuffer.class);
+        verify(client).marshall(argumentCaptor.capture());
+        final ByteBuffer wireMessage = argumentCaptor.getValue();
         assertThat(wireMessage.capacity()).isEqualTo(8 +//object+size+opcode
                                                      4 +//int arg
                                                      4 + 12 +//string arg (size + int aligned chars)
--- server/java/westfield.server/westfield.server.runtime/src/test/java/org/freedesktop/westfield/server/WConnectionTest.java
+++ server/java/westfield.server/westfield.server.runtime/src/test/java/org/freedesktop/westfield/server/WConnectionTest.java
@@ -8,11 +8,6 @@ import org.mockito.InjectMocks;
 import org.mockito.internal.util.reflection.Whitebox;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import javax.websocket.CloseReason;
-import javax.websocket.Session;
-import java.util.HashMap;
-import java.util.Map;
-
 import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
@@ -28,80 +23,19 @@ public class WConnectionTest {
     @Test
     public void onOpen() throws Exception {
         //given
-        final Session session = mock(Session.class);
-        when(session.getNegotiatedSubprotocol()).thenReturn("westfield");
-
         final WRegistry         wRegistry         = mock(WRegistry.class);
         final WRegistryResource wRegistryResource = mock(WRegistryResource.class);
         when(wRegistry.createResource(any(WClient.class))).thenReturn(wRegistryResource);
         Whitebox.setInternalState(this.wConnection,
                                   "registry",
                                   wRegistry);
+        WSender wSender = mock(WSender.class);
 
         //when
-        this.wConnection.onOpen(session);
+        this.wConnection.create(wSender);
 
         //then
         assertThat(this.wConnection.getClients()).hasSize(1);
         verify(wRegistry).publishGlobals(wRegistryResource);
-    }
-
-    @Test
-    public void onOpenBadSubprotocol() throws Exception {
-        //given
-        final Session session = mock(Session.class);
-        when(session.getNegotiatedSubprotocol()).thenReturn("testfield");
-
-        //when
-        this.wConnection.onOpen(session);
-
-        //then
-        final ArgumentCaptor<CloseReason> reasonArgumentCaptor = ArgumentCaptor.forClass(CloseReason.class);
-        verify(session).close(reasonArgumentCaptor.capture());
-
-        assertThat(reasonArgumentCaptor.getValue()
-                                       .getCloseCode()).isEqualTo(CloseReason.CloseCodes.PROTOCOL_ERROR);
-    }
-
-    @Test
-    public void onError() throws Exception {
-        //given
-        final Session               session = mock(Session.class);
-        final WClient               wClient = mock(WClient.class);
-        final Map<Session, WClient> clients = new HashMap<>();
-        clients.put(session,
-                    wClient);
-        Whitebox.setInternalState(this.wConnection,
-                                  "wClients",
-                                  clients);
-
-        final Throwable t = mock(Throwable.class);
-
-        //when
-        this.wConnection.onError(t,
-                                 session);
-
-        //then
-        verify(wClient).on(t);
-    }
-
-    @Test
-    public void onClose() throws Exception {
-        //given
-        final Session               session = mock(Session.class);
-        final WClient               wClient = mock(WClient.class);
-        final Map<Session, WClient> clients = new HashMap<>();
-        clients.put(session,
-                    wClient);
-        Whitebox.setInternalState(this.wConnection,
-                                  "wClients",
-                                  clients);
-
-        //when
-        this.wConnection.onClose(session);
-
-        //then
-        assertThat(this.wConnection.getClients()).isEmpty();
-        verify(wClient).onClose();
     }
 }
