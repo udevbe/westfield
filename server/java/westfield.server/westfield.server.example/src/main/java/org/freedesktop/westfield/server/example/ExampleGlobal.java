20199a9235ffd232b8c8c8127b026e21f2821415
Erik De Rijcke <derijcke.erik@gmail.com>
2017-01-07 00:14:54 +0100

    begin defining example protocol and implementation

--- server/java/westfield.server/westfield.server.example/src/main/java/org/freedesktop/westfield/server/example/ExampleGlobal.java
+++ server/java/westfield.server/westfield.server.example/src/main/java/org/freedesktop/westfield/server/example/ExampleGlobal.java
@@ -4,7 +4,7 @@ package org.freedesktop.westfield.server.example;
 import org.freedesktop.westfield.server.WClient;
 import org.freedesktop.westfield.server.WGlobal;
 
-public class ExampleGlobal extends WGlobal {
+public class ExampleGlobal extends WGlobal implements ExampleGlobalRequests {
 
     public ExampleGlobal() {
         super(ExampleGlobal.class.getSimpleName(),
@@ -15,6 +15,18 @@ public class ExampleGlobal extends WGlobal {
     public void bindClient(final WClient client,
                            final int id,
                            final int version) {
+        new ExampleGlobalResource(client,
+                                  version,
+                                  id,
+                                  this);
+    }
 
+    @Override
+    public void createExampleClock(final ExampleGlobalResource resource,
+                                   final int id) {
+        new ExampleClockResource(resource.getClient(),
+                                 resource.getVersion(),
+                                 id,
+                                 new ExampleClockRequests() {});
     }
 }
---
+++ server/java/westfield.server/westfield.server.example/src/main/java/org/freedesktop/westfield/server/example/package-info.java
@@ -0,0 +1,5 @@
+@Protocols(@Protocol("example.xml"))
+package org.freedesktop.westfield.server.example;
+
+import org.freedesktop.westfield.generator.api.Protocol;
+import org.freedesktop.westfield.generator.api.Protocols;
