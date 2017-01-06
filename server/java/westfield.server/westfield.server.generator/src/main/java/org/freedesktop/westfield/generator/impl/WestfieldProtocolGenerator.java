c5488bdf226bd2be04449c50bad35e03304324a5
Erik De Rijcke <derijcke.erik@gmail.com>
2017-01-07 00:12:54 +0100

    support java8 sources

--- server/java/westfield.server/westfield.server.generator/src/main/java/org/freedesktop/westfield/generator/impl/WestfieldProtocolGenerator.java
+++ server/java/westfield.server/westfield.server.generator/src/main/java/org/freedesktop/westfield/generator/impl/WestfieldProtocolGenerator.java
@@ -5,12 +5,18 @@ import com.google.auto.service.AutoService;
 
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
 import java.util.Collections;
 
 @AutoService(Processor.class)
 @SupportedAnnotationTypes({"org.freedesktop.westfield.generator.api.Protocols"})
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class WestfieldProtocolGenerator extends BasicAnnotationProcessor {
+
     protected Iterable<? extends ProcessingStep> initSteps() {
         return Collections.singleton(new ProtocolsProcessingStep(this.processingEnv));
     }
+
+
 }
