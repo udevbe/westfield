9cacb648412b3cb6d2a188edf8d30fb291076afa
Erik De Rijcke <derijcke.erik@gmail.com>
2017-01-10 15:02:08 +0100

    remove westfield server protocol sub module

--- server/java/westfield.server/westfield.server.protocol/src/main/java/org/freedesktop/westfield/package-info.java
+++
@@ -1,5 +0,0 @@
-@Protocols(@Protocol("westfield.xml"))
-package org.freedesktop.westfield;
-
-import org.freedesktop.westfield.generator.api.Protocol;
-import org.freedesktop.westfield.generator.api.Protocols;
