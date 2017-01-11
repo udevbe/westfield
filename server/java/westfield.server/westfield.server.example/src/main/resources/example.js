c59840dc5b953fbd910bdc91cebec8bc320dc784
Erik De Rijcke <derijcke.erik@gmail.com>
2017-01-11 17:01:23 +0100

    add comments to js example

--- server/java/westfield.server/westfield.server.example/src/main/resources/example.js
+++ server/java/westfield.server/westfield.server.example/src/main/resources/example.js
@@ -1,11 +1,11 @@
 "use strict";
-const connection = new wfc.Connection("ws://127.0.0.1:8080/westfield");
-connection.registry.listener.global = (name, interface_, version) => {
-    if (interface_ === "example_global") {
-        const exampleGlobal = connection.registry.bind(name, interface_, version);
-        const exampleClock = exampleGlobal.create_example_clock();
-        exampleClock.listener.time_update = (time) => {
-            document.getElementById("the_time").innerHTML = time.toString();
+const connection = new wfc.Connection("ws://127.0.0.1:8080/westfield");//connect to the server
+connection.registry.listener.global = (name, interface_, version) => {//register a listener to will be notified if a new global appears
+    if (interface_ === "example_global") {//check if we support the global
+        const exampleGlobal = connection.registry.bind(name, interface_, version);//create a new object that will be bound to the global
+        const exampleClock = exampleGlobal.create_example_clock();//create a new clock object
+        exampleClock.listener.time_update = (time) => {//listen for time updates
+            document.getElementById("the_time").innerHTML = time.toString();//show the time
         };
     }
 };
