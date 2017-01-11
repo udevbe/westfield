e9ebd66491d2f1249ab5aaa4267d01a4eade97e1
Erik De Rijcke <derijcke.erik@gmail.com>
2017-01-11 11:22:03 +0100

    make example more compact

--- server/java/westfield.server/westfield.server.example/src/main/resources/example.js
+++ server/java/westfield.server/westfield.server.example/src/main/resources/example.js
@@ -1,19 +1,11 @@
+"use strict";
 const connection = new wfc.Connection("ws://127.0.0.1:8080/westfield");
-
-let exampleGlobal;
-let exampleClock;
-
-function onTimeUpdate(time) {
-    const date = new Date(time);
-    document.getElementById("the_time").innerHTML = "Server time UTC: "+date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "." + date.getMilliseconds();
-}
-
-function onGlobal(name, interface_, version) {
+connection.registry.listener.global = (name, interface_, version) => {
     if (interface_ === "example_global") {
-        exampleGlobal = connection.registry.bind(name, interface_, version);
-        exampleClock = exampleGlobal.create_example_clock();
-        exampleClock.listener.time_update = onTimeUpdate;
+        const exampleGlobal = connection.registry.bind(name, interface_, version);
+        const exampleClock = exampleGlobal.create_example_clock();
+        exampleClock.listener.time_update = (time) => {
+            document.getElementById("the_time").innerHTML = time.toString();
+        };
     }
-    //else unknown/unsupported global
-}
-connection.registry.listener.global = onGlobal;
+};
