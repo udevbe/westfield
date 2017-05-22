98fecbbf0b639165e9d22b5d23f6d7f67bf60370
Erik De Rijcke <derijcke.erik@gmail.com>
2017-05-22 17:24:39 +0200

    wip - c/p client js implementation

--- server/node/example/public.src/compositor-client.js
+++ server/node/example/public.src/compositor-client.js
@@ -1,2 +1,13 @@
+"use strict";
 const wfc = require("./westfield-client-example.js");
 
+const connection = new wfc.Connection("ws://127.0.0.1:8080/westfield");//connect to the server
+connection.registry.listener.global = (name, interface_, version) => {//register a listener to will be notified if a new global appears
+    if (interface_ === "example_global") {//check if we support the global
+        const exampleGlobal = connection.registry.bind(name, interface_, version);//create a new object that will be bound to the global
+        const exampleClock = exampleGlobal.create_example_clock();//create a new clock object
+        exampleClock.listener.time_update = (time) => {//listen for time updates
+            document.getElementById("the_time").innerHTML = time.toString();//show the time
+        };
+    }
+};
