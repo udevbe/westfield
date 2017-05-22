40498f63339f825b0309cd5b1d098e32c2200040
Erik De Rijcke <derijcke.erik@gmail.com>
2017-05-22 17:25:01 +0200

    wip - serve static html & init simple websocket server

--- server/node/example/compositor-server.js
+++ server/node/example/compositor-server.js
@@ -1,15 +1,31 @@
 #!/usr/bin/env node
 'use strict';
-
-
 const wfs = require('./westfield-server-example.js');
 const WebSocket = require('ws');
-
-const wss = new WebSocket.Server({ port: 8080 });
-
-
+const express = require('express');
+const http = require('http');
 
 
+const app = express();
+app.use(express.static('public'));
+
+const server = http.createServer();
+server.on('request', app);
+
+const wss = new WebSocket.Server({
+    server: server,
+    path: "/westfield"
+});
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
+
+    ws.send('something');
+});
+
+server.listen(8080);
 
 
 
