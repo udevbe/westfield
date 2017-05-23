db55abdbf3cdac9c22e266eee7d4b4c2939bd6fe
Erik De Rijcke <derijcke.erik@gmail.com>
2017-05-23 13:12:13 +0200

    wip - more server side impl wip

--- server/node/example/compositor-server.js
+++ server/node/example/compositor-server.js
@@ -1,35 +1,44 @@
 #!/usr/bin/env node
 'use strict';
+
 const wfs = require('./westfield-server-example.js');
 const WebSocket = require('ws');
 const express = require('express');
 const http = require('http');
 
-
-const app = express();
-app.use(express.static('public'));
-
-const server = http.createServer();
-server.on('request', app);
-
-const wss = new WebSocket.Server({
-    server: server,
-    path: "/westfield"
-});
-
-//create westfield server
+//create westfield server.
 const wfsServer = new wfs.Server();
 
 //create global clock factory implementation
 const exampleGlobal = new wfs.Global("example_global", 1);
 exampleGlobal.bindClient = function (client, id, version) {
+    //create an example global resource when a client binds to it.
     const resource = new wfs.example_global(client, id, version);
-    resource.implementation.create_example_clock = (id) => {
-
-    };
+    //assign factory method
+    resource.implementation.create_example_clock = createExampleClock;
 };
 
+//implement factory method
+function createExampleClock(id) {
+    const clockResource = new wfs.example_clock(this.client, id, 1);
+
+    setInterval(function () {
+        clockResource.implementation.time_update(new Date().getTime());
+    }, 1);
+}
+
 wfsServer.registry.register(exampleGlobal);
+
+
+//setup connection logic
+app.use(express.static('public'));
+
+const server = http.createServer();
+server.on('request', app);
+const wss = new WebSocket.Server({
+    server: server,
+    path: "/westfield"
+});
 
 wss.on('connection', function connection(ws) {
 
@@ -38,14 +47,27 @@ wss.on('connection', function connection(ws) {
     const client = wfsServer.createClient();
 
     client.doSend = function (wireMsg) {
-        ws.send(wireMsg, function (error) {
+        try {
+            ws.send(wireMsg, function (error) {
+                if (error !== undefined) {
+                    console.error(error);
+                    client.onClose();
+                }
+            });
+        } catch (error) {
             console.error(error);
             client.onClose();
-        });
+        }
     };
 
+
     ws.on('message', function incoming(message) {
-        client.onReceive(message);
+        try {
+            client.onReceive(message);
+        } catch (error) {
+            console.error(error);
+            client.onClose();
+        }
     });
 
     ws.on('close', function () {
