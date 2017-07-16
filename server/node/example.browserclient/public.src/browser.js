718dfa486e933d2fcaa941d50f2c0f9cba9a19ad
Erik De Rijcke <derijcke.erik@gmail.com>
2017-07-16 23:59:11 +0200

    add comment

--- server/node/example.browserclient/public.src/browser.js
+++ server/node/example.browserclient/public.src/browser.js
@@ -13,7 +13,7 @@ ws.onmessage = (event) => {
 }// wire websocket message to connection unmarshall
 
 ws.onopen = (event) => {
-  const registry = connection.createRegistry()
+  const registry = connection.createRegistry() // create a registry that will notify us of any current and new globals
   registry.listener.global = (name, interface_, version) => { // register a listener to will be notified if a new global appears
     if (interface_ === 'example_global') { // check if we support the global
       const exampleGlobal = registry.bind(name, interface_, version)// create a new object that will be bound to the global
