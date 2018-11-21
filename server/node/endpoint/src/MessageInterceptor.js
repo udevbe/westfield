9b7b2768986a4a57e946be5dda22b9d463d6e8a7
Erik De Rijcke <derijcke.erik@gmail.com>
2018-11-21 10:57:41 +0100

    remove message event handling

--- server/node/endpoint/src/MessageInterceptor.js
+++ server/node/endpoint/src/MessageInterceptor.js
@@ -29,22 +29,6 @@ class MessageInterceptor {
   /**
    * @param {number}objectId
    * @param {number}opcode
-   * @param {Uint32Array}messageBuffer
-   */
-  handleEvent (objectId, opcode, messageBuffer) {
-    if (objectId === 1 && opcode === 2) {
-      const deleteObjectId = messageBuffer[2]
-      const interceptor = this.interceptors[deleteObjectId]
-      Endpoint.destroyWlResourceSilently(interceptor.wlResource)
-      delete this.interceptors[deleteObjectId]
-      return
-    }
-
-  }
-
-  /**
-   * @param {number}objectId
-   * @param {number}opcode
    * @param {{buffer: ArrayBuffer, fds: Array<number>, bufferOffset: number, consumed: number, size: number}}message
    * @return {number} where the message should be send to. 0 = browser only, 1 native only, 2 both.
    */
